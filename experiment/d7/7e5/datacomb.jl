8f2dc7c6d1c02f365e3c9feff1d03e21d3bcc508
AWS ParallelCluster user <ec2-user@ip-172-31-25-194.us-east-2.compute.internal>
2021-06-14 18:37:46 +0000

    ilovegithub

---
+++ experiment/d7/7e5/datacomb.jl
@@ -0,0 +1,43 @@
+using DelimitedFiles
+#using StatsBase
+
+function comb()
+
+dx = 3
+dz = 7
+p = "0.00007"
+num_sim = 9
+
+nfails = []
+ntrials = []
+failure_rate = []
+
+
+failtot=0
+trialtot=0
+
+
+for sim_id in 1:num_sim
+  fname = "experiment_dz$(dz)_dx$(dx)_p$(p)_id$(sim_id).txt"
+  fin = open(fname,"r")
+  data = readdlm(fin)
+  n = Int(data[1])
+  f = Int(data[2])
+  failtot = Int(failtot+f)
+  trialtot = Int(trialtot+n)
+  push!(failure_rate, f/n)
+  close(fin)
+end
+
+fname="combdata_dz$(dz)_dx$(dx)_p$(p)_id.txt"
+fout=open(fname,"w")
+writedlm(fout,[failtot,trialtot,failtot/trialtot])
+close(fout)
+
+end
+
+comb()
+
+#@show mean(failure_rate)
+#@show sem(failure rate
+
